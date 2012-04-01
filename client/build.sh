3d380f3d4b42492734f34b06b08eaa16bb018291
Nick Niemeir <nick.niemeir@gmail.com>
2012-04-01 04:07:06 +0000

    Getting the rest of the libraries into build script.
    Adding a couple of more tests

--- client/build.sh
+++ client/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 coffee -c lib/*.coffee
-cat lib/utility.js > client.js
+cat lib/jquery-1.7.1.min.js > client.js
+cat lib/jquery-ui-1.8.16.custom.min.js >> client.js
+cat lib/underscore-min.js >> client.js
+cat lib/dataDash.js >> client.js
+cat lib/utility.js >> client.js
+cp client.js test/testclient.js
 cat lib/run.js >> client.js
--- client/test/main.js
+++ client/test/main.js
@@ -3,5 +3,19 @@ describe('utility.js', function () {
     it('should make the wiki global object', function () {
       expect(wiki).to.exist;
     });
+    it('should get a randomByte()', function () {
+      var a = wiki.randomByte();
+      expect(a).to.be.a('string');
+      expect(a.length).to.be(2);
+    });
+    it('should get n randomBytes(n)', function () {
+      var a = wiki.randomBytes(8);
+      expect(a).to.be.a('string');
+      expect(a.length).to.be(16);
+    });
+    it('should slugify a name', function () {
+      var a = wiki.asSlug('Welcome Visitors');
+      expect(a).to.be('welcome-visitors');
+    });
   });
 });
